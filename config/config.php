29426a27a2e917767c0f39cfe6bb9ad67cedb5ef
Karel Kozlík <kozlik@kufr.cz>
2019-05-31 22:09:11 +0200

    Directory /var/lib/serweb-frmwrk/smarty/ might be created with wrong permissions (#14)

--- config/config.php
+++ config/config.php
@@ -9,18 +9,18 @@
     $config->iquest->notifications = array();
     /**
      *  Configure send email notifications when team find a solution
-     *  
-     *  $config->iquest->notifications['solution-id'] = array("foo@bar.cz", 
-     *                                                        "bagr@bagr.com");           
-     */         
-    
+     *
+     *  $config->iquest->notifications['solution-id'] = array("foo@bar.cz",
+     *                                                        "bagr@bagr.com");
+     */
 
-    /** 
+
+    /**
      *  Default timezone:
-     *         
+     *
      *  List of supported timezones:
      *  http://www.php.net/manual/en/timezones.php
-     */ 
+     */
     $config->timezone = "Europe/Prague";
 
 
@@ -38,8 +38,8 @@
     $config->use_rpc = false;
 
     $config->force_lang = 'cs-utf-8';
-    
-    // Following is needed by function remove_diacritics(). It should be set to 
+
+    // Following is needed by function remove_diacritics(). It should be set to
     // some UTF8 charset installed on the system.
     $config->iquest_locale = 'cs_CZ.UTF-8';
 
@@ -47,13 +47,15 @@
     $config->img_src_path =     $config->root_path."img/";
     $config->iquest_data_dir = realpath($_SERWEB["approotdir"]."data")."/";
 
+    // Directory where smarty stores compiled templates
+    $config->smarty_compile_dir = realpath($_SERWEB["approotdir"]."smarty")."/";
+
     $config->enable_logging = true;
     $config->log_file = "/var/log/iquest";
     $config->log_level = "PEAR_LOG_DEBUG";
-    
+
     $config->custom_act_log_function = "iquest_action_log";
 
     $config->html_doctype = "html";
-    
+
     $config->html_headers[] = "<!--[if lt IE 9]><LINK REL=\"StyleSheet\" HREF=\"".htmlspecialchars($config->style_src_path."ie.css")."\" TYPE=\"text/css\" /><![endif]-->\n";
-?>
